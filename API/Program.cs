ff7a97db8bc01a4ee906498cf149c688de2b7095
Steven Ryland <steven.ryland@infinicept.com>
2022-10-07 16:40:01 -0700

    Add HttpClient

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Options;
 using Prometheus;
+using TeslaGateway_PrometheusProxy;
 using TeslaGateway_PrometheusProxy.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,12 +10,23 @@ builder.Services.AddControllers();
 builder.Services.AddMemoryCache();
 builder.Services.Configure<LoginRequest>(builder.Configuration.GetSection("TeslaGateway"));
 builder.Services.Configure<TeslaGatewaySettings>(builder.Configuration.GetSection("TeslaGateway"));
-builder.Services.AddSingleton<CollectorRegistry>(c =>
+builder.Services.AddHttpClient(nameof(TeslaGatewayMetricsService), (_, client) => 
+{
+    client.BaseAddress = new Uri($"https://{builder.Configuration["TeslaGateway:Host"]}");
+    client.DefaultRequestHeaders.Host = builder.Configuration["TeslaGateway:Host"];
+}).ConfigurePrimaryHttpMessageHandler(_ =>
+{
+    var handler = new HttpClientHandler();
+    handler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
+    return handler;
+});
+builder.Services.AddSingleton<TeslaGatewayMetricsService>();
+builder.Services.AddSingleton<CollectorRegistry>(_ =>
 {
     var registry = Metrics.NewCustomRegistry();
     registry.SetStaticLabels(new Dictionary<string, string>()
     {
-        { "Host", c.GetRequiredService<IOptions<TeslaGatewaySettings>>().Value.Host }
+        { "Host", builder.Configuration["TeslaGateway:Host"] }
     });
     return registry;
 });
