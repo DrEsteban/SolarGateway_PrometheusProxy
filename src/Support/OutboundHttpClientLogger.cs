1956385f98f71994984f06ebe0cceae2355cd17a
Steven Ryland <stevenry@outlook.com>
2024-11-14 13:19:32 -0700

    Update outbound http logger impl

--- src/Support/OutboundHttpClientLogger.cs
+++ src/Support/OutboundHttpClientLogger.cs
@@ -8,28 +8,36 @@ public class OutboundHttpClientLogger(ILogger<OutboundHttpClientLogger> _logger)
     {
         _logger.LogError(
             exception,
-            "Request '{Request.Host}{Request.Path}' failed after {Response.ElapsedMilliseconds}ms",
+            "Request '{Request.Host}{Request.Path}' failed after {Response.ElapsedMilliseconds}ms w/ status '{Response.StatusCodeInt}'",
             request.RequestUri?.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped),
-            request.RequestUri!.PathAndQuery,
-            elapsed.TotalMilliseconds.ToString("F1"));
+            request.RequestUri?.PathAndQuery,
+            elapsed.TotalMilliseconds.ToString("F1"),
+            (int?)response?.StatusCode);
     }
 
     public object? LogRequestStart(HttpRequestMessage request)
     {
-        _logger.LogInformation(
+        _logger.LogTrace(
             "Sending '{Request.Method}' to '{Request.Host}{Request.Path}'",
             request.Method,
             request.RequestUri?.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped),
-            request.RequestUri!.PathAndQuery);
+            request.RequestUri?.PathAndQuery);
         return null;
     }
 
     public void LogRequestStop(object? context, HttpRequestMessage request, HttpResponseMessage response, TimeSpan elapsed)
     {
-        _logger.LogInformation(
-            "Received '{Response.StatusCodeInt} {Response.StatusCodeString}' after {Response.ElapsedMilliseconds}ms",
+        var severity = response.IsSuccessStatusCode ? LogLevel.Debug : LogLevel.Information;
+        _logger.Log(
+            severity,
+            "Received '{Response.StatusCodeInt} {Response.StatusCodeString}' " +
+            "for request '{Request.Method} {Request.Host}{Request.Path}' " +
+            "after {Response.ElapsedMilliseconds}ms",
             (int)response.StatusCode,
             response.StatusCode,
+            request.Method,
+            request.RequestUri?.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped),
+            request.RequestUri?.PathAndQuery,
             elapsed.TotalMilliseconds.ToString("F1"));
     }
 }
