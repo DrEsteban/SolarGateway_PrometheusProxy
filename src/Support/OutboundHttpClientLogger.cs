ccc35b16d8ade9f6914278ec4f64f5589e2520cf
Steven Ryland <stevenry@outlook.com>
2024-11-23 17:58:05 -0700

    Minor update to OutboundHttpClientLogger.cs

--- src/Support/OutboundHttpClientLogger.cs
+++ src/Support/OutboundHttpClientLogger.cs
@@ -6,27 +6,45 @@ public class OutboundHttpClientLogger(ILogger<OutboundHttpClientLogger> _logger)
 {
     public void LogRequestFailed(object? context, HttpRequestMessage request, HttpResponseMessage? response, Exception exception, TimeSpan elapsed)
     {
-        _logger.LogError(
-            exception,
-            "Request '{Request.Host}{Request.Path}' failed after {Response.ElapsedMilliseconds}ms w/ status '{Response.StatusCodeInt}'",
-            request.RequestUri?.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped),
-            request.RequestUri?.PathAndQuery,
-            elapsed.TotalMilliseconds.ToString("F1"),
-            (int?)response?.StatusCode);
+        (string? host, string? path) = GetRequestInfo(request);
+        string elapsedMsString = GetElapsedMsString(elapsed);
+
+        if (exception is OperationCanceledException)
+        {
+            _logger.LogWarning(
+                "Request '{Request.Host}{Request.Path}' was canceled after {Response.ElapsedMilliseconds}ms",
+                host,
+                path,
+                elapsedMsString);
+        }
+        else
+        {
+            _logger.LogError(
+                exception,
+                "Request '{Request.Method} {Request.Host}{Request.Path}' failed after {Response.ElapsedMilliseconds}ms w/ status '{Response.StatusCodeInt}'",
+                request.Method,
+                host,
+                path,
+                elapsedMsString,
+                (int?)response?.StatusCode);
+        }
     }
 
     public object? LogRequestStart(HttpRequestMessage request)
     {
+        (string? host, string? path) = GetRequestInfo(request);
         _logger.LogTrace(
             "Sending '{Request.Method}' to '{Request.Host}{Request.Path}'",
             request.Method,
-            request.RequestUri?.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped),
-            request.RequestUri?.PathAndQuery);
+            host,
+            path);
         return null;
     }
 
     public void LogRequestStop(object? context, HttpRequestMessage request, HttpResponseMessage response, TimeSpan elapsed)
     {
+        (string? host, string? path) = GetRequestInfo(request);
+        string elapsedMsString = GetElapsedMsString(elapsed);
         var severity = response.IsSuccessStatusCode ? LogLevel.Debug : LogLevel.Information;
         _logger.Log(
             severity,
@@ -36,8 +54,15 @@ public class OutboundHttpClientLogger(ILogger<OutboundHttpClientLogger> _logger)
             (int)response.StatusCode,
             response.StatusCode,
             request.Method,
-            request.RequestUri?.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped),
-            request.RequestUri?.PathAndQuery,
-            elapsed.TotalMilliseconds.ToString("F1"));
+            host,
+            path,
+            elapsedMsString);
     }
+
+    private static (string? host, string? path) GetRequestInfo(HttpRequestMessage request)
+        => (request.RequestUri?.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped),
+            request.RequestUri?.PathAndQuery);
+
+    private static string GetElapsedMsString(TimeSpan elapsed)
+        => elapsed.TotalMilliseconds.ToString("F1");
 }
